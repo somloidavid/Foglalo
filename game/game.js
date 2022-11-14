20eb7aef01bfb8a869500933a924a2a0f77cc481
somloi.david <somloi.david@students.jedlik.eu>
2022-11-14 09:06:17 +0100

    randomizálás (unfinished)

--- game/game.js
+++ game/game.js
@@ -1,5 +1,30 @@
-function Answer(){
-    //tömböket tartalmazó lista
-    //mindig az első a jó
-    //index alapjan csekkeli h jó e
+class Question{
+    constructor(q){
+        const randb = Math.floor(Math.random() * 2) + 1;
+        this.question = q[0];
+        this.correct = q[1];
+        this.first = q[];
+        this.second = ;
+        this.third = ;
+    }
+}
+
+const input = [["Ki Kriszhadvice?","Tanács Krisztián","A prediction-ök démonja","A Béke Szigetének őrzője"], ["?","A","B","C"], ["!", "1", "2", "3"]];
+const questions = [];
+
+input.forEach(q => {
+    questions.push(new Question(q));
+});
+
+function Popup(){
+    const randq = Math.floor(Math.random() * input.length);
+    document.getElementById("question").innerText = questions[randq].question;
+    document.getElementById("ch1").innerText = questions[randq];
+}
+
+function Answer(a){
+    if (a.innerText == questions[0].correct) {
+        
+    }
+
 }
