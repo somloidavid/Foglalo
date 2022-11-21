58a79cad9e545256a1d485b7f2e3efd9a6af10d9
Papp Adam <papp.adam@students.jedlik.eu>
2022-11-21 09:35:59 +0100

    working renderer + mouse pos

--- game/game.js
+++ game/game.js
@@ -1,5 +1,5 @@
 class Obj {
-    constructor(index, x, y, width, height) {
+    constructor(index, x, y, width, height, dst_cam) {
         this.imgSrc = index;
         this.pos = {
             x: x,
@@ -10,33 +10,68 @@ class Obj {
             width: width,
             height: height,
         }
+
+        this.distance_from_cam = dst_cam;
     }
 }
 
+let canvas;
+let ctx;
+let imgs;
+let objects;
 
-function draw(ctx, objs, imgs) {
-    for (let i = 0; i < objs.length; i++) {
-        let obj = objs[i];
-        ctx.drawImage(imgs[obj.imgSrc], obj.pos.x, obj.pos.y);
+let mouse = {
+    x: 0,
+    y: 0,
+}
+
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    for (let i = 0; i < objects.length; i++) {
+        let obj = objects[i];
+        ctx.drawImage(imgs[obj.imgSrc], obj.pos.x / obj.distance_from_cam, obj.pos.y / obj.distance_from_cam, obj.size.width / obj.distance_from_cam, obj.size.height / obj.distance_from_cam);
     }
+}
+
+function createNewImg(path) {
+    let htmlImg = document.createElement("IMG");
+    htmlImg.src = path;
+    htmlImg.style.filter = 'brightness(0.2)';
+    document.body.appendChild(htmlImg);
+
+    return htmlImg;
 }
 
 function main() {
-    let canvas = document.getElementById("main-canvas");
+    canvas = document.getElementById("main-canvas");
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
-    let ctx = canvas.getContext("2d");
 
-    let imgs = [
-        document.getElementById("kep"), // ind: 0
+    ctx = canvas.getContext("2d");
+
+    imgs = [
+        createNewImg("../img/boojg.png"),
     ];
 
-    let objects = [
-        new Obj(0, 0, 0, 256, 256),
-        new Obj(0, 256, 256, 256, 256),
+    objects = [
+        new Obj(0, 1024, 128, 256, 256, 1.3),
+        new Obj(0, 0, 0, 256, 256, 1),
     ];
 
-    draw(ctx, objects, imgs);
+    window.requestAnimationFrame(loop)
+
 }
 
-window.onload = main;
+function loop() {
+    draw();
+
+    window.requestAnimationFrame(loop)
+}
+
+var lastRender = 0
+window.onload = main
+window.addEventListener("mousemove", function(event) {
+    mouse.x = event.clientX;
+    mouse.y = event.clientY;
+});
