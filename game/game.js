ac6553ab8eeb45e1e3f457fd572febe9eebedc63
Pozsgai Bence <pozsgai.bence@students.jedlik.eu>
2022-11-25 11:21:38 +0100

    szornyu a helyzet , bajban a haza (semmi nem joXD)

---
+++ game/game.js
@@ -0,0 +1,10 @@
+function inspect(a){
+    console.log(a)
+    let elso = document.getElementById("elso");
+    let masodik = document.getElementById("masodik");
+    let harmadik = document.getElementById("harmadik");
+    let negyedik = document.getElementById("negyedik");
+    
+
+    }
+}
