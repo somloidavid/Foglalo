79e24a0ef8a35f7ccc1507e0d593ca6b234429e4
somloi.david <somloi.david@students.jedlik.eu>
2022-11-25 12:27:51 +0100

    timer, válaszellenőrzés

--- game/game.js
+++ game/game.js
@@ -8,7 +8,7 @@ class Question{
     }
 }
 
-const input = [["Ki Kriszhadvice?","Tanács Krisztián","A prediction-ök démonja","A Béke Szigetének őrzője"], ["?","A","B","C"], ["!", "1", "2", "3"]];
+const input = [["Ki Kriszhadvice?","Tanács Krisztián","A prediction-ök démonja","A Béke Szigetének őrzője"], ["Milyen méretű Herby cigarettája?","Közepes","Kis","Nagy"], ["Melyik egy chatbot neve?", "Málik Irén", "Stohl András", "Ben Dover"], ["Mi lett az L-ből az ismert népzene szerint?", "W", "N", "Szalonna"]];
 const questions = [];
 
 input.forEach(q => {
@@ -19,30 +19,59 @@ let randq;
 
 function Popup(){
     document.getElementById("popup").style.visibility = "visible";
+    document.getElementById("popup").style.opacity = "1";
     randq = questions[Math.floor(Math.random() * questions.length)];
-    console.log(randq)
     document.getElementById("question").innerHTML = randq.question;
-    let r_index = randq.answers[Math.floor(Math.random() * randq.answers.length)];
-    console.log(randq.answers[r_index]);
-    document.getElementById("ch1").innerHTML = `<p>${randq.answers[r_index]}</p>`;
-    randq.answers.splice(r_index);
-    r_index = randq.answers[Math.floor(Math.random() * randq.answers.length)];
-    document.getElementById("ch2").innerHTML = `<p>${randq.answers.pop(Math.floor(Math.random() * randq.answers.length))}</p>`;
-    randq.answers.splice(r_index);
-    r_index = randq.answers[Math.floor(Math.random() * randq.answers.length)];
-    document.getElementById("ch3").innerHTML = `<p>${randq.answers.pop(Math.floor(Math.random() * randq.answers.length))}</p>`;
-    randq.answers.splice(r_index);
+    document.getElementById("ch1").innerHTML = `<p>${randq.answers.splice(Math.random() * randq.answers.length, 1)}</p>`;
+    document.getElementById("ch2").innerHTML = `<p>${randq.answers.splice(Math.random() * randq.answers.length, 1)}</p>`;
+    document.getElementById("ch3").innerHTML = `<p>${randq.answers.splice(Math.random() * randq.answers.length, 1)}</p>`;
+    let timer = document.getElementById("timer");
+    let interval = setInterval(() => {
+        timer.innerHTML = `<p>${parseInt(timer.innerText) - 1}</p>`;
+        console.log(interval)
+        if (timer.innerText <= 0){
+            clearInterval(interval);
+            OffTimer();
+        }
+    }, 1000);
 }
 
+let answered = false;
 function Answer(a){
-    console.log(a.innerHTML)
-    console.log(`<p>${randq.correct}</p>`)
-    if (a.innerHTML == `<p>${randq.correct}</p>`) {
-        document.getElementById(a.id).style.backgroundColor = "rgb(0,255,0)";
+    if (!answered) {
+        document.getElementById(a.id).style.border = "thick solid orange";
+        answered = true;
+        if (a.innerHTML == `<p>${randq.correct}</p>`) {
+            setTimeout(() => {
+                document.getElementById(a.id).style.backgroundColor = "rgb(0,255,0)";
+              }, 1000);
+        }
+        else{
+            setTimeout(() => {
+                document.getElementById(a.id).style.backgroundColor = "rgb(255,0,0)";
+            }, 1000);
+        }
     }
-    else{
-        document.getElementById(a.id).style.backgroundColor = "rgb(255,0,0)";
-    }
+}
+
+function OffTimer(){
+    let main = document.getElementById("main");
+    main.innerHTML = `<div id="notif"></div>`;
+    let notif = document.getElementById("notif");
+    notif.style.width = "55%";
+    notif.style.height = "30vh";
+    notif.style.margin = "35vh auto";
+    notif.style.backgroundColor = "red";
+    notif.style.opacity = "0";
+    notif.style.transition = "opacity 1s";
+    notif.innerHTML = `<div id="OffTimerBox"><p id="OffTimerText">Lejárt az idő!</p></div>`
+    let OffTimerBox = document.getElementById("OffTimerBox");
+    OffTimerBox.style.margin = "12vh auto";
+    OffTimerBox.style.height = "6vh";
+    OffTimerBox.style.top = "10vh";
+    OffTimerBox.style.position = "relative";
+    let OffTimerText = document.getElementById("OffTimerText");
+    OffTimerText.style.fontSize = "6vh";
 }
 
 Popup();
