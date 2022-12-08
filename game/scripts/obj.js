0d78731afedd1de919bcef640b216ccf9c84f3bb
somloidavid <somloi.david@students.jedlik.eu>
2022-12-08 18:23:15 +0100

    hp ismét jó

--- game/scripts/obj.js
+++ game/scripts/obj.js
@@ -28,7 +28,8 @@ class Obj {
             this.planetInfoRaw.push('<p style="color: rgb(74, 228, 163);">Staus: Ally</p>');
         }
         else
-            this.planetInfoRaw.push('<p style="color: rgb(228, 74, 74);">Staus: Enemy</p>');
+        this.planetInfoRaw.push('<p style="color: rgb(228, 74, 74);">Staus: Enemy</p>');
+        this.planetInfoRaw.push(`<p style="color: rgb(228, 74, 74);" id="hp">${this.hp}/${this.question_limit}</p>`);
         this.planetInfo = this.infoToStr();
 
         this.question_limit = limit;
